c5a773685b29e24b5d958fbf2ca918451ff24f76
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-21 03:56:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+RegexProvider
+=============
+
+The RegexProvider project contains a type providers for regular expresissions. <a href="http://fsprojects.github.io/RegexProvider" target="_blank">Documentation available here.</a>
+
+## Building
+
+* Run build.cmd
---
+++ src/RegexProvider/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("RegexProvider")>]
+[<assembly: AssemblyProductAttribute("RegexProvider")>]
+[<assembly: AssemblyDescriptionAttribute("A type provider for regular expressions.")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
