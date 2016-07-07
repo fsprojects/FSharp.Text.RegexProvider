999912ebc22bdd013c8c4476b8fc2f1ba194e881
Vasily Kirichenko <vaskir@gmail.com>
2016-07-07 20:50:51 +0300

    bump version to 1.0.0

--- src/RegexProvider/AssemblyInfo.fs
+++ src/RegexProvider/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Text.RegexProvider")>]
 [<assembly: AssemblyProductAttribute("FSharp.Text.RegexProvider")>]
 [<assembly: AssemblyDescriptionAttribute("A type provider for regular expressions.")>]
-[<assembly: AssemblyVersionAttribute("0.0.7")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.7")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.7"
+    let [<Literal>] Version = "1.0.0"
